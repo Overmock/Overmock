ccf76f7cf67a45389d88befd09f00dfa18dcbf87
sbaker <sbaker@logikbug.com>
2023-08-10 18:54:08 -0700

    adding di package

---
+++ src/Overmock.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Overmock.Proxies;
+
+namespace Overmock.Examples
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection AddScopedProxy<TInterface, TImplementation>(this IServiceCollection services, Action<InvocationContext> memberInvoked)
+			where TInterface : class
+			where TImplementation : class, TInterface
+		{
+			services.TryAddScoped<TImplementation>();
+			return services.AddScoped<TInterface>(s =>
+				Interceptor.Intercept<TInterface, TImplementation>(s.GetRequiredService<TImplementation>(), memberInvoked));
+		}
+	}
+}
